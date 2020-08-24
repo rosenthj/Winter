fcc100b8000fd9f1c0efd4a6ec0f2093bcb2f27f
Jonathan Rosenthal <rosenthj@ethz.ch>
2020-08-24 17:06:52 -0400

    take 2 Bench 1570439

--- src/general/settings.h
+++ src/general/settings.h
@@ -33,7 +33,7 @@
 namespace settings {
 
 const std::string engine_name = "Winter";
-const std::string engine_version = "0.8.4a";
+const std::string engine_version = "200824";
 const std::string engine_author = "Jonathan Rosenthal";
 
 #if defined(__BMI2__)
