3e9d2eb3d295560f1c8b3d21b77ba318352d0036
Jonathan Rosenthal <rosenthj@ethz.ch>
2020-08-26 03:33:53 -0400

    Removed src/general/debug.*

--- src/uci.cc
+++ src/uci.cc
@@ -33,7 +33,6 @@
 #include "net_evaluation.h"
 #include "search.h"
 #include "transposition.h"
-#include "general/debug.h"
 #include "search_thread.h"
 #include <cstdint>
 #include <vector>
@@ -146,7 +145,6 @@ void Reply(std::string message) {
 namespace uci {
 
 void Loop() {
-  debug::EnterFunction(debug::kUci, "uci::Loop", "");
   Board board;
   std::string in;
   while (std::getline(std::cin, in)) {
@@ -351,7 +349,9 @@ void Loop() {
       std::vector<Move> moves = board.GetMoves<kNonQuiescent>();
       uint64_t sum = 0;
       Time begin = now();
+#ifndef NDEBUG
       HashType hash = board.get_hash();
+#endif
       for (Move move : moves) {
         board.Make(move);
         long perft_result = search::Perft(board, depth-1);
@@ -365,9 +365,7 @@ void Loop() {
       auto time_used = std::chrono::duration_cast<Milliseconds>(end-begin);
       std::cout << "depth: " << depth << " perft: " << sum << " time: " << time_used.count()
           << " nps: " << ((sum * 1000) / (time_used.count() + 1)) << std::endl;
-      if (board.get_hash() != hash) {
-        debug::Error("Hash after perft function is changed!", false);
-      }
+      assert(board.get_hash() == hash);
     }
 #ifdef BOOKKEEPING
     else if (Equals(command, "bookkeeping_reset")) {
@@ -417,7 +415,6 @@ void Loop() {
       Reply("Received unknown command: " + command);
     }
   }
-  debug::ExitFunction(debug::kUci);
 }
 
 }
