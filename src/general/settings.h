672c5f200acf66c082195f3eda9c38fd0822ca77
Jonathan Rosenthal <rosenthj@ethz.ch>
2020-09-04 02:20:41 -0400

    Updated version number Bench 1148884

--- src/general/settings.h
+++ src/general/settings.h
@@ -33,7 +33,7 @@
 namespace settings {
 
 const std::string engine_name = "Winter";
-const std::string engine_version = "0.8.6";
+const std::string engine_version = "0.8.7";
 const std::string engine_author = "Jonathan Rosenthal";
 
 #if defined(__BMI2__)
