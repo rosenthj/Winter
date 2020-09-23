2e5414beaabb44248c1812593292acfb58514c64
Jonathan Rosenthal <rosenth0@purdue.edu>
2020-09-22 23:16:55 -0400

    Time management v3 (#26)
    Reworked time management formula to something more intuitive and clean.
    
    ELO   | 19.35 +- 7.61 (95%)
    SPRT  | 60.0+0.6s Threads=1 Hash=64MB
    LLR   | 2.95 (-2.94, 2.94) [0.00, 4.00]
    Games | N: 3019 W: 654 L: 486 D: 1879
    
    ELO   | 27.27 +- 10.12 (95%)
    SPRT  | 12.0+0.04s Threads=1 Hash=8MB
    LLR   | 2.96 (-2.94, 2.94) [0.00, 4.00]
    Games | N: 2336 W: 693 L: 510 D: 1133
    
    ELO   | 10.00 +- 5.80 (95%)
    SPRT  | 40/10.0s Threads=1 Hash=8MB
    LLR   | 2.96 (-2.94, 2.94) [0.00, 4.00]
    Games | N: 7089 W: 1928 L: 1724 D: 3437
    
    * Easier to understand time management function
    * Use time more aggressively
    * Even more aggressive time usage formula
    * Updated version number
    Bench 811188

--- src/uci.cc
+++ src/uci.cc
@@ -33,6 +33,7 @@
 #include "search.h"
 #include "transposition.h"
 #include "search_thread.h"
+#include <array>
 #include <cstdint>
 #include <vector>
 #include <sstream>
@@ -49,6 +50,16 @@ std::vector<std::string> &splits(const std::string &s, char delimeter,
     elements.push_back(item);
   }
   return elements;
+}
+
+// Returns a maximum time with margin of error for param clock time
+int32_t  maxtime(int32_t time) {
+  return std::max((8 * time) / 10, time - 100);
+}
+
+
+int32_t  get_base_time(int32_t  base, int32_t  inc, int32_t  to_go=22) {
+  return std::min(maxtime(base),((58 * base) / std::min(50, to_go) + 58 * inc) / 50);
 }
 
 std::vector<std::string> split(const std::string &s, char delim) {
@@ -95,10 +106,6 @@ std::vector<UCIOption> uci_options {
   {"LMRMultiplierPV", search::SetLMRMultiplierPV, 76, 0, 100},
   {"LMROffsetPVCap", search::SetLMROffsetPVCap, 50, -155, 100},
 //  {"LMRMultiplierPVCap", search::SetLMRMultiplierPVCap, 23, 0, 100},
-//  {"LMP1", search::SetLMP1, 6, 1, 30},
-//  {"LMP2", search::SetLMP2, 7, 1, 30},
-//  {"LMP3", search::SetLMP3, 8, 1, 30},
-//  {"LMP4", search::SetLMP4, 22, 1, 30},
   {"LMPBaseNW", search::SetLMPBaseNW, 3, 1, 20},
   {"LMPBasePV", search::SetLMPBasePV, 5, 1, 20},
   {"LMPScalar", search::SetLMPScalar, 12, 0, 24},
@@ -112,24 +119,13 @@ const std::string kEngineAuthorPrefix = "id author ";
 const std::string kOk = "uciok";
 
 struct Timer {
-  Timer() {
-    for (Color color = kWhite; color <= kBlack; color++) {
-      time[color] = 0;
-      inc[color] = 0;
-    }
-    movetime = 0;
-    moves_to_go = 0;
-    search_depth = 0;
-    nodes = 0;
-  }
-  int time[2], inc[2], movetime;
+  std::array<int, 2> time;
+  std::array<int, 2> inc;
+  int movetime;
   size_t nodes;
-  Depth moves_to_go, search_depth;
+  Depth moves_to_go;
+  Depth search_depth;
 };
-
-int maxtime(int time) {
-  return std::max((8 * time) / 10, time - 100);
-}
 
 void Go(Board *board, Timer timer) {
   Move move = 0;
@@ -144,16 +140,15 @@ void Go(Board *board, Timer timer) {
     move = search::NodeSearch((*board), timer.nodes);
   }
   else {
-    if (timer.moves_to_go == 0) {
-      timer.moves_to_go = 40;
+    int time;
+    const Color color = board->get_turn();
+    if (timer.moves_to_go > 0) {
+      time = get_base_time(timer.time[color], timer.inc[color], timer.moves_to_go);
     }
-    Color color = board->get_turn();
-    int time = timer.time[color] + ((timer.time[color] * board->get_phase()) / kMaxPhase);
-    time = (6 * time) / 5;
-    time /= timer.moves_to_go + 8;
-    time = std::max(time, timer.time[color] / timer.moves_to_go + 4);
-    time += timer.inc[color];
-    Milliseconds duration = Milliseconds(maxtime(time));
+    else {
+      time = get_base_time(timer.time[color], timer.inc[color]);
+    }
+    Milliseconds duration = Milliseconds(time);
     move = search::TimeSearch((*board), duration);
   }
   std::cout << "bestmove " << parse::MoveToString(move) << std::endl;
@@ -294,7 +289,7 @@ void Loop() {
     }
     else if (Equals(command, "go")) {
       search::end_search();
-      Timer timer;
+      Timer timer {};
       if (tokens.size() >= index+2) {
         while (tokens.size() >= index+2) {
           std::string arg = tokens[index++];
