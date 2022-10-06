c274702557af08e8b5c93999bca7493cd64b962e
Jonathan Rosenthal <rosenth0@purdue.edu>
2022-10-06 19:58:43 -0400

    Updated net version Bench 1294027

--- src/general/settings.h
+++ src/general/settings.h
@@ -33,7 +33,7 @@
 namespace settings {
 
 const std::string engine_name = "Winter";
-const std::string engine_version = "0.9.13";
+const std::string engine_version = "0.9.14";
 const std::string engine_author = "Jonathan Rosenthal";
 
 #if defined(__BMI2__)
