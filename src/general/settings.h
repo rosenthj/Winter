ca172d08b2c12c45f9b1f1685d0ca2f7d7dd15f1
Jonathan Rosenthal <rosenth0@purdue.edu>
2022-10-13 11:36:28 -0400

    Net 221012 (#57)
    * Updated net
    * Updated version number
    
    ELO   | 3.88 +- 3.13 (95%)
    SPRT  | 60.0+0.60s Threads=1 Hash=64MB
    LLR   | 2.95 (-2.94, 2.94) [0.00, 5.00]
    GAMES | N: 24456 W: 6458 L: 6185 D: 11813
    
    ELO   | 4.15 +- 3.34 (95%)
    SPRT  | 10.0+0.10s Threads=1 Hash=8MB
    LLR   | 2.94 (-2.94, 2.94) [0.00, 5.00]
    GAMES | N: 25712 W: 8148 L: 7841 D: 9723
    
    Bench 1235871

--- src/general/settings.h
+++ src/general/settings.h
@@ -33,7 +33,7 @@
 namespace settings {
 
 const std::string engine_name = "Winter";
-const std::string engine_version = "0.9.18";
+const std::string engine_version = "0.9.19";
 const std::string engine_author = "Jonathan Rosenthal";
 
 #if defined(__BMI2__)
