2efb992d4affd0eaba199bf9ed8fa218861000f3
Jonathan Rosenthal <rosenth0@purdue.edu>
2022-10-19 13:23:40 -0400

    Net 221018 (#66)
    * Updated net
    * Version update
    
    ELO   | 3.27 +- 2.59 (95%)
    SPRT  | 60.0+0.60s Threads=1 Hash=64MB
    LLR   | 2.97 (-2.94, 2.94) [0.00, 5.00]
    GAMES | N: 35560 W: 9353 L: 9018 D: 17189
    
    ELO   | 5.78 +- 4.44 (95%)
    SPRT  | 10.0+0.10s Threads=1 Hash=8MB
    LLR   | 3.01 (-2.94, 2.94) [0.00, 5.00]
    GAMES | N: 14192 W: 4409 L: 4173 D: 5610
    
    Bench 1093437

--- src/net_evaluation.cc
+++ src/net_evaluation.cc
@@ -6,7 +6,7 @@
 #include <vector>
 #include <cmath>
 
-INCBIN(float_t, NetWeights, "f160rS04_ep7.bin");
+INCBIN(float_t, NetWeights, "f160rS05_ep7.bin");
 
 // NN types
 constexpr size_t block_size = 160;
