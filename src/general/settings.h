e7c2d4379dc629bf5670ac2fa9c9f74a8366dbe5
Jonathan Rosenthal <rosenth0@purdue.edu>
2022-11-14 22:25:26 -0500

    Version number updated Bench 1112400

--- src/general/settings.h
+++ src/general/settings.h
@@ -33,7 +33,7 @@
 namespace settings {
 
 const std::string engine_name = "Winter";
-const std::string engine_version = "1.08";
+const std::string engine_version = "1.09";
 
 const std::string engine_author = "Jonathan Rosenthal";
 
