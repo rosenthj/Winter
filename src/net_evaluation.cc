5e4509cdec25a2951314a04bb87086303c1f38aa
Jonathan Rosenthal <rosenth0@purdue.edu>
2022-12-08 08:24:19 -0500

    Net update Bench 1166700

--- src/net_evaluation.cc
+++ src/net_evaluation.cc
@@ -6,7 +6,7 @@
 #include <vector>
 #include <cmath>
 
-INCBIN(float_t, NetWeights, "g224rS02_ep4.bin");
+INCBIN(float_t, NetWeights, "g256rS01_ep4.bin");
 //INCBIN(float_t, NetWeights, "f224rS15_ep4.bin");
 //INCBIN(float_t, NetWeights, "f192rS12_ep4.bin");
 //INCBIN(float_t, NetWeights, "f256G32rS01b_ep3.bin");
@@ -14,9 +14,9 @@ INCBIN(float_t, NetWeights, "g224rS02_ep4.bin");
 //INCBIN(float_t, NetWeights, "f224rS08_ep3.bin");
 
 // NN types
-constexpr size_t block_size = 224;
+constexpr size_t block_size = 256;
 using NetLayerType = Vec<float, block_size>;
-constexpr size_t reduced_block_size = 224;
+constexpr size_t reduced_block_size = 256;
 using ReducedNetLayerType = Vec<float, reduced_block_size>;
 
 std::array<int32_t, 2> contempt = { 0, 0 };
