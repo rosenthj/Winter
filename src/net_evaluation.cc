39fc0bba31e36869a5d0eb9741579932808f8c2a
Jonathan Rosenthal <rosenth0@purdue.edu>
2023-03-30 00:52:23 -0400

    Net update Bench 1053900

--- src/net_evaluation.cc
+++ src/net_evaluation.cc
@@ -6,7 +6,7 @@
 #include <vector>
 #include <cmath>
 
-INCBIN(float_t, NetWeights, "i256rS01_ep6.bin");
+INCBIN(float_t, NetWeights, "h256rS07_ep4.bin");
 //INCBIN(float_t, NetWeights, "h224rS02_ep4.bin");
 
 // NN types
