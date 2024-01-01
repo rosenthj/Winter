269da9d851d75b50c4a5466fc84a7bc531c50012
Jonathan Rosenthal <rosenth0@purdue.edu>
2024-01-01 04:18:11 -0500

    Net 231231 (#111)
    * Retrained net
    * Version number update
    
    Elo   | 18.00 +- 8.95 (95%)
    SPRT  | 60.0+0.60s Threads=1 Hash=64MB
    LLR   | 2.95 (-2.94, 2.94) [0.00, 5.00]
    Games | N: 2704 W: 702 L: 562 D: 1440
    Penta | [32, 293, 601, 355, 71]
    http://chess.grantnet.us/test/34914/
    
    Elo   | 18.27 +- 9.10 (95%)
    SPRT  | 10.0+0.10s Threads=1 Hash=8MB
    LLR   | 2.96 (-2.94, 2.94) [0.00, 5.00]
    Games | N: 3122 W: 952 L: 788 D: 1382
    Penta | [84, 345, 585, 417, 130]
    http://chess.grantnet.us/test/34913/
    
    Bench 1110400

--- src/general/settings.h
+++ src/general/settings.h
@@ -33,7 +33,7 @@
 namespace settings {
 
 const std::string engine_name = "Winter";
-const std::string engine_version = "2.07";
+const std::string engine_version = "2.07a";
 
 const std::string engine_author = "Jonathan Rosenthal";
 
