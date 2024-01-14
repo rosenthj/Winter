ee4365be39cb1114222de3c55503b7cc7ccc3521
rosenthj <rosenth0@purdue.edu>
2024-01-14 08:32:26 -0500

    New H96 net has promising stats, so it is getting tested Bench 1097800

--- src/general/settings.h
+++ src/general/settings.h
@@ -33,7 +33,7 @@
 namespace settings {
 
 const std::string engine_name = "Winter";
-const std::string engine_version = "2.07d";
+const std::string engine_version = "2.07d RN8H96a";
 
 const std::string engine_author = "Jonathan Rosenthal";
 
