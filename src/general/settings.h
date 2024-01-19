41483edd01353ddd31d4c04152c05395c1982141
rosenthj <rosenth0@purdue.edu>
2024-01-19 18:44:33 -0500

    Testing a new net Bench 1075300

--- src/general/settings.h
+++ src/general/settings.h
@@ -33,7 +33,7 @@
 namespace settings {
 
 const std::string engine_name = "Winter";
-const std::string engine_version = "2.07e RN16H64f";
+const std::string engine_version = "2.07e RN8H96c";
 
 const std::string engine_author = "Jonathan Rosenthal";
 
--- src/net_evaluation.cc
+++ src/net_evaluation.cc
@@ -7,15 +7,15 @@
 #include <cmath>
 #include <list>
 
-INCBIN(float_t, NetWeights, "rnet16H64f.bin");
-//INCBIN(float_t, NetWeights, "rnet8H96.bin");
+//INCBIN(float_t, NetWeights, "rnet16H64f.bin");
+INCBIN(float_t, NetWeights, "rnet8H96c.bin");
 //INCBIN(float_t, NetWeights, "rnet8H64g.bin");
 
 // NN types
-constexpr size_t block_size = 16;
+constexpr size_t block_size = 8;
 using NetLayerType = Vec<float_t, block_size>;
 
-constexpr size_t full_block_size = 64;
+constexpr size_t full_block_size = 96;
 using FullLayerType = Vec<float_t, full_block_size>;
 
 std::array<int32_t, 2> contempt = { 0, 0 };
