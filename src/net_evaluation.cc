5b5c0cfe4b536250fde1be875d61c1da3b9caf4d
Jonathan Rosenthal <rosenth0@purdue.edu>
2024-02-02 19:02:19 -0500

    Net 240127 (#120)
    * Network update
    * Version number update to release version
    
    Regression test vs v2.0 with 8moves opening book:
    Elo   | 119.24 +- 7.98 (95%)
    Conf  | 40.0+0.40s Threads=1 Hash=64MB
    Games | N: 4002 W: 1742 L: 420 D: 1840
    Penta | [24, 166, 637, 812, 362]
    http://chess.grantnet.us/test/35205/
    
    Elo   | 5.71 +- 4.43 (95%)
    SPRT  | 60.0+0.60s Threads=1 Hash=64MB
    LLR   | 2.99 (-2.94, 2.94) [0.00, 5.00]
    Games | N: 9986 W: 2194 L: 2030 D: 5762
    Penta | [137, 1081, 2400, 1231, 144]
    http://chess.grantnet.us/test/35199/
    
    Elo   | 9.19 +- 6.17 (95%)
    SPRT  | 10.0+0.10s Threads=1 Hash=8MB
    LLR   | 2.97 (-2.94, 2.94) [0.00, 5.00]
    Games | N: 6240 W: 1685 L: 1520 D: 3035
    Penta | [116, 741, 1305, 778, 180]
    http://chess.grantnet.us/test/35194/
    
    Bench 764100

--- src/net_evaluation.cc
+++ src/net_evaluation.cc
@@ -7,9 +7,9 @@
 #include <cmath>
 #include <list>
 
-//INCBIN(float_t, NetWeights, "rnet16H64f.bin");
-//INCBIN(float_t, NetWeights, "rnet8H96c.bin");
-INCBIN(float_t, NetWeights, "rnet8H64i.bin");
+//INCBIN(float_t, NetWeights, "rnet16H64g.bin");
+//INCBIN(float_t, NetWeights, "rnet8H96e.bin");
+INCBIN(float_t, NetWeights, "rnet8H64j.bin");
 
 // NN types
 constexpr size_t block_size = 8;
