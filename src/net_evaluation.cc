36989cd700c49cce586d90565f6af2982e6edfc3
rosenthj <rosenth0@purdue.edu>
2024-02-21 21:41:10 -0500

    Testing another interation of the 16H64 net. Hopefully competitive with the mirrored net. Bench 821000

--- src/net_evaluation.cc
+++ src/net_evaluation.cc
@@ -7,12 +7,12 @@
 #include <cmath>
 #include <list>
 
-//INCBIN(float_t, NetWeights, "rnet16H64h.bin");
+INCBIN(float_t, NetWeights, "rnet16H64i.bin");
 //INCBIN(float_t, NetWeights, "rnet8H96e.bin");
-INCBIN(float_t, NetWeights, "rnet8H64k.bin");
+//INCBIN(float_t, NetWeights, "rnet8H64k.bin");
 
 // NN types
-constexpr size_t block_size = 8;
+constexpr size_t block_size = 16;
 using NetLayerType = Vec<float_t, block_size>;
 
 constexpr size_t full_block_size = 64;
