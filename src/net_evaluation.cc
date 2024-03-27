2ea92402211ec5ec81e447f219ba4b7dec72e0d4
rosenthj <rosenth0@purdue.edu>
2024-03-27 16:30:25 -0400

    Testing with slightly lower regularization Bench 860400

--- src/net_evaluation.cc
+++ src/net_evaluation.cc
@@ -9,7 +9,7 @@
 
 //INCBIN(float_t, NetWeights, "rnet16H64h.bin");
 //INCBIN(float_t, NetWeights, "rnet8H96e.bin");
-INCBIN(float_t, NetWeights, "rnet8H64oL1em5.bin");
+INCBIN(float_t, NetWeights, "rnet8H64oL1em6.bin");
 
 // NN types
 constexpr size_t block_size = 8;
