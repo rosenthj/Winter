657b4f85bce34ca42eeb89ba4741df3a146e0eb2
rosenthj <rosenth0@purdue.edu>
2024-06-12 00:30:04 -0400

    One last attempt at adjusting quiescent move ordering Bench 557300

--- src/move_order.cc
+++ src/move_order.cc
@@ -127,27 +127,28 @@ struct Sorter {
   //~ //return 1001;
 //~ }
 
+constexpr std::array<MoveScore, 7> v{1, 3, 3, 5, 10, 0, 0};
+
 MoveScore get_move_priority(const Move move, search::Thread &t,
                             const Square last_destination) {
   const MoveType move_type = GetMoveType(move);
   if (!IsQuiet(move)) {
+    constexpr MoveScore base = 160;
     const Piece piece = t.board.get_piece(GetMoveSource(move));
     const Square des = GetMoveDestination(move);
     const PieceType piece_type = t.board.get_piece_type(des);
     if (move_type == kCapture) {
       int32_t capture_score = t.get_capture_score(piece, des, piece_type);
-      return (1 << 9) + (1 << 10) * piece_type + capture_score;
+      return (1 << 9) + base * v[piece_type] + capture_score;
     }
     if (move_type == kEnPassant) {
       int32_t capture_score = t.get_capture_score(piece, des, kPawn);
-      return (1 << 9) + capture_score;
+      return (1 << 9) + base + capture_score;
     }
     int32_t capture_score = t.get_capture_score(piece, des, piece_type);
-    const int32_t promo_value = move_type - kKnightPromotion + kKnight;
-    if (piece_type == kNoPiece) {
-      return (1 << 9) + (1 << 10) * promo_value + capture_score;
-    }
-    return (1 << 9) + (1 << 10) * (promo_value + piece_type) + capture_score;
+    MoveScore promo_value = v[move_type - kKnightPromotion + kKnight];
+    promo_value += v[piece_type];
+    return (1 << 9) + base * promo_value + capture_score;
   }
   return 20 + t.get_history_score(t.board.get_turn(), GetMoveSource(move), GetMoveDestination(move)) / 1024;
   //assert(GetMoveType(move) == kEnPassant);
