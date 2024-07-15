6f0241aa8f1d7ebc5728655f7ea6382d1687ead7
rosenthj <rosenth0@purdue.edu>
2024-07-15 02:29:28 -0400

    Net update Bench 451000

--- src/net_evaluation.cc
+++ src/net_evaluation.cc
@@ -13,7 +13,7 @@
 //INCBIN(float_t, NetWeights, "rnet16H64h.bin");
 //INCBIN(float_t, NetWeights, "rnet8H96e.bin");
 //INCBIN(float_t, NetWeights, "rnet16H64o.bin");
-INCBIN(float_t, NetWeights, "rn16HD64a2.bin");
+INCBIN(float_t, NetWeights, "rn16HD64b.bin");
 
 std::array<int32_t, 2> contempt = { 0, 0 };
 
