043873f0e707a9147c9b2c2f4d18a2f7cd889cba
rosenthj <rosenth0@purdue.edu>
2026-01-06 21:30:01 -0500

    Even more aggressive smoothening Bench 443600

--- src/search_thread.cc
+++ src/search_thread.cc
@@ -162,10 +162,10 @@ Score Thread::adjust_static_eval(const Score static_eval) const {
   float win = static_eval.get_win_probability();
   float loss = static_eval.get_loss_probability();
   
-  constexpr float alpha = 1.5f;
+  constexpr float alpha = 0.5f;
   constexpr float divisor = 8192.0f;
-  win += fast_tanh(alpha * win_error / divisor) / alpha;
-  loss += fast_tanh(alpha * loss_error / divisor) / alpha;
+  win += fast_tanh(win_error / (alpha * divisor)) * alpha;
+  loss += fast_tanh(loss_error / (alpha * divisor)) * alpha;
   
   win = sclamp(win, 0.0f, 1.0f);
   loss = sclamp(loss, 0.0f, 1.0f);
