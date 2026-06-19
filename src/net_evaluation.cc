fb70b729bcb7d2dda7c00d0a1fcc7f34a9c8a343
rosenthj <rosenth0@purdue.edu>
2026-06-19 17:25:07 -0400

    Next attempt. Bench 434100

--- src/net_evaluation.cc
+++ src/net_evaluation.cc
@@ -13,7 +13,7 @@
 //INCBIN(float_t, NetWeights, "rnet16H64h.bin");
 //INCBIN(float_t, NetWeights, "rnet8H96e.bin");
 //INCBIN(float_t, NetWeights, "rnet16H64o.bin");
-INCBIN(float_t, NetWeights, "rn16HD64_t03.bin");
+INCBIN(float_t, NetWeights, "rn16HD64_t04.bin");
 
 std::array<int32_t, 2> contempt = { 0, 0 };
 
