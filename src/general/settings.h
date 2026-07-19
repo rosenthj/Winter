6f7653a731f2c552d160133458d1194b8cfe43a7
rosenthj <rosenth0@purdue.edu>
2026-07-19 14:44:40 -0400

    Another way to create the dataset. Bench 463800

--- src/general/settings.h
+++ src/general/settings.h
@@ -33,7 +33,7 @@
 namespace settings {
 
 const std::string engine_name = "Winter";
-const std::string engine_version = "4.02 AdvB";
+const std::string engine_version = "4.02 AdvF";
 
 const std::string engine_author = "Jonathan Rosenthal";
 
