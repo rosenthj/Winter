92d502a3745035e87284067f2ee73ec62720c8ed
rosenthj <rosenth0@purdue.edu>
2026-08-11 03:38:12 -0400

    Slight reduction in dataset size, but hopefully higher quality for training. Using WSD scheduler instead of linear. Hoping for small rating gain! Bench 1406000

--- src/net_evaluation.cc
+++ src/net_evaluation.cc
@@ -10,10 +10,7 @@
 #include <cmath>
 #include <list>
 
-//INCBIN(float_t, NetWeights, "rnet16H64h.bin");
-//INCBIN(float_t, NetWeights, "rnet8H96e.bin");
-//INCBIN(float_t, NetWeights, "rnet16H64o.bin");
-INCBIN(float_t, NetWeights, "rn16HD64_rew501iE_ep36.bin");
+INCBIN(float_t, NetWeights, "rn16HD64_rew502E2_ep36.bin");
 
 std::array<int32_t, 2> contempt = { 0, 0 };
 
