345de7f8c04e1331cba25913c809e3a01334dbc4
rosenthj <rosenth0@purdue.edu>
2026-08-14 12:08:47 -0400

    Trained net with more traditional scheduler. Bench 1226000

--- src/net_evaluation.cc
+++ src/net_evaluation.cc
@@ -10,7 +10,7 @@
 #include <cmath>
 #include <list>
 
-INCBIN(float_t, NetWeights, "rn16HD64_rew503_ep36.bin");
+INCBIN(float_t, NetWeights, "rn16HD64_rew503E_ep36.bin");
 
 std::array<int32_t, 2> contempt = { 0, 0 };
 
