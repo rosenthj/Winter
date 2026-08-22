8bfe1bfffdfaf8c0c444c1ac2a9273c205e02306
rosenthj <rosenth0@purdue.edu>
2026-08-22 08:09:36 -0400

    Larger net Bench 1450800

--- src/net_evaluation.cc
+++ src/net_evaluation.cc
@@ -29,7 +29,7 @@
 #include <cmath>
 #include <list>
 
-INCBIN(float_t, NetWeights, "rn16HD64_rew503_W50_ep40.bin");
+INCBIN(float_t, NetWeights, "rn24HD64_rew503_d24f64_ep40.bin");
 
 std::array<int32_t, 2> contempt = { 0, 0 };
 
